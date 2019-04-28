b29d61b0740dadcc4dec992fdf3925d5b2a7165e
nalarkin <nathanlarkin.usa@gmail.com>
2019-04-28 15:14:21 -0700

    added general workflow description, added a  function to import team compositions, added pseudo-code for future functions

--- listImport.py
+++ listImport.py
@@ -1,37 +1,100 @@
 """
-TODO: Make list of comps that are viable, make the list of unit names in each
-TODO: Use console to input units you have into a list
-TODO: Select a archetype to follow using console
-TODO: Given you select this archetype and have these units, recommend which
-    units you are looking for to complete build
-TODO: Sugguest which comp you should go based on the least amount of units
-        you would need to buy to complete synergies
-
-
-
-
+This program keeps track of suggests team comps and keeps track of user
+pieces they input. Then it displays the units they need to buy to complete
+composition.
+"""
 
 """
+Workflow:
+
+- user enters units they currently have
+- program suggests comps
+- user selects comp
+- program prints full comp
+- program prints the pieces they need to complete comp
+- user updates pieces they have
+- program retrieves new list of pieces to print
+"""
+
+
 import pandas, os
-os.chdir("C:\\Users\\Nathan\\Downloads")
 
 
 def create_database():
+    os.chdir("C:\\Users\\Nathan\\Downloads")
     return pandas.read_csv("AutoChess_units.csv", index_col=0)
 
 
-def create_unit_list():
-    create_unit_dict = {}
+def import_compositions():
+    os.chdir("C:\\MyPythonScripts\\autochess-assistant\\autochess-assistant")
+    return pandas.read_csv("end_game_compositions.csv", index_col=0)
+
+
+def create_unit_dict():
+    unit_dict = {}
     unit_index = df.index
     for unit in unit_index:
         spec1 = df.loc[unit, "Spec"]
         spec2 = df.loc[unit, 'Unnamed: 2']
         synergy = df.loc[unit, 'Class']
-        create_unit_dict[unit] = [[spec1, spec2], synergy]
-    return create_unit_dict
+        unit_dict[unit] = [[spec1, spec2], synergy]
+    return unit_dict
+
+
+def create_composition_dict():
+    comp_dict = {}
+    comp_index = compositions.index
+    for comp in comp_index:
+        if comp != "S" and comp != "A":
+            comp_dict[comp] = compositions.loc[comp, "Unnamed: 1"]
+    return comp_dict
+
+
+def request_units():
+    # "user enters units separated by ,"
+    print("Which Comp will you play? ")
+    # add to a list
+    # return list of owned_units
+    pass
+
+
+def suggest_comps():
+    # traverse through complete_comp_dict, if any key matches a unit they
+    # they have, add the comp name to a new list
+    # return list called potential_comps
+    pass
+
+def select_comps():
+    # prompt user to select comp they want to play (they can respond with
+    # numbers 1, 2, 3, etc. which corresponds to index[] + 1
+    # save the comp name they choose as a variable string object
+    # return selected_comp
+    pass
+
+
+def initialize_comp():
+    # use selected_comp to search complete_comp_dictionary's list of units
+    # add all units to a list
+    # print selected comp units (full list)
+    # return list
+    pass
+
+
+def retrieve_pieces():
+    # list of units in comp - owned_units = units_needed
+    # return units_needed
+    pass
+
+
+def update_pieces():
+    # ask user to type whenever they get a new unit that is part of the comp
+    # add unit to the owned_units list
+    pass
 
 
 df = create_database()
-unit_dict = create_unit_list()
-print(unit_dict)
-
+compositions = import_compositions()
+complete_unit_dict = create_unit_dict()
+complete_comp_dict = create_composition_dict()
+print(complete_comp_dict)
+print(complete_unit_dict)
