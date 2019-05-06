512f937d8a39c9a89b26b18b14405e6405fa66d1
nalarkin <nathanlarkin.usa@gmail.com>
2019-05-05 21:35:02 -0700

    update 0.23
    ordered comp selection choices, fixed storage of current units so now the 'units needed' is accurate.

--- listImport.py
+++ listImport.py
@@ -43,7 +43,7 @@ def import_compositions():
     return pandas.read_csv("end_game_compositions.csv", index_col=0)
 
 
-def create_unit_dict():
+def create_unit_dict(unit_dataframe):
     unit_dict = {}
     unit_index = unit_dataframe.index
     for unit in unit_index:
@@ -54,7 +54,7 @@ def create_unit_dict():
     return unit_dict
 
 
-def create_composition_dict():
+def create_composition_dict(compositions):
     comp_dict = {}
     comp_index = compositions.index
     for comp in comp_index:
@@ -67,14 +67,17 @@ def create_composition_dict():
 def request_units(unit_list=None):
     if unit_list:       # used to update a list of units that already exists
         print()
-        input_current_units = unit_list + input("Add more units here: ").split(
+        response = input("Add more units here, or type "
+                         "'s' to stop: "
+                         "").split(
                 sep=', ')
+        if response[0] == 's':
+            return None
+        input_current_units = unit_list + response
         print()
-
     else:           # to create a new list
         input_current_units = input("What units do you current have? (separate by "
                            "comma) ").split(sep=', ')
-
     return input_current_units
 
 
@@ -86,16 +89,16 @@ def suggest_comps():
 
 
 # find available comps the user can play with given pieces
-def find_comps():
-    remaining_comps = {}
+def find_comps(complete_comp_dict):
+    remaining_comps = []
     for comp, comp_units in complete_comp_dict.items():
         matching_unit_counter = 0
         for owned_unit in current_units:
             if owned_unit in comp_units:
                 matching_unit_counter += 1
         if matching_unit_counter > 0:
-            remaining_comps[comp] = matching_unit_counter
-    # print("remaining comps = {}".format(remaining_comps))
+            remaining_comps.append((matching_unit_counter, comp))
+    remaining_comps.sort(reverse=True)
     return remaining_comps
 
 
@@ -103,23 +106,21 @@ def find_comps():
 # numbers 1, 2, 3, etc. which corresponds to index[] + 1
 # save the  name they choose as a variable string object
 # return final_comp
-def select_comp():
+def select_comp(remaining_comps):
     counter = 1
     print()
-    for comp, matching_unit_counter in remaining_comps.items():
-        print("#{0}: {1},  {2} matching units".format(str(counter), comp,
-                                                      matching_unit_counter))
+    for comp in remaining_comps:
+        print("#{0}: {1}-----{2} matching units".format(str(counter), comp[1],
+                                                        comp[0]))
 
         counter += 1
     print()
     response = int(input("Which comp would you like to play (enter number)? "))
     print()
-    final_comp_list = list(remaining_comps.keys())
-    selected_comp = final_comp_list[response - 1]
-    # for units in remaining_comps[selected_comp]:
-    #     final_comp.append(units)
-    final_comp = [remaining_comps[selected_comp]]
-
+    selected_comp = remaining_comps[response - 1]       # returns tuple (
+                                                        # matching units , comp name)
+    selected_comp = selected_comp[1]        # selects the comp name out of tuple
+    final_comp = complete_comp_dict[selected_comp]
     return final_comp
 
 
@@ -129,9 +130,8 @@ def select_comp():
 # return list
 def initialize_comp():
     # print("Final Comp is: " + str(final_comp))
-    final_unit_list = final_comp
-    return final_unit_list
-
+    print("final comp is = {}".format(final_comp))
+    print()
 
 # list of units in comp - owned_units = units_needed
 # return units_needed
@@ -143,14 +143,17 @@ def retrieve_pieces():
     print("Units left  = {}".format(str(final_comp)))
     return final_comp
 
-def keep_updating(current_units):
+def keep_updating(current_units, final_comp):
     response = ""
-    print("current units: {}".format(current_units))
-    while response != "s":
-        current_units = request_units(current_units)
-        print("updated current units: {}".format(current_units))
+    # print("current units: {}".format(current_units))
+    while current_units:
+        actual_final_comp = []
+        for unit in final_comp:
+            if not current_units.__contains__(unit):
+                actual_final_comp.append(unit)
+        print("You need these units!{}".format(actual_final_comp))
         print()
-        response = input("type anything to continue, type 's' to stop: ")
+        current_units = request_units(current_units)
 
 def check_units():
     #     for unit in current_units:
@@ -163,15 +166,15 @@ def check_units():
 
 unit_dataframe = create_database()  # pandas imported csv file
 compositions = import_compositions()        # pandas imported csv file
-complete_unit_dict = create_unit_dict()     # dictionary that contains all units
+complete_unit_dict = create_unit_dict(unit_dataframe)     # dictionary that contains all units
                                             # with synergys and class
-complete_comp_dict = create_composition_dict()  # dict (composition, units) with meta
+complete_comp_dict = create_composition_dict(compositions)  # dict (composition, units) with meta
                                                 #  unit compositions
 current_units = request_units()     # first time user is requested for units
-remaining_comps = find_comps()      # find available comps
-final_comp = select_comp()      # give user choice in selecting comp
+remaining_comps = find_comps(complete_comp_dict)      # find available comps
+final_comp = select_comp(remaining_comps)      # give user choice in selecting comp
 # progress_list = []
-final_unit_list = initialize_comp()
-keep_updating(current_units)        # process runs until user types 's'
+initialize_comp()
+keep_updating(current_units, final_comp)        # process runs until user types 's'
 # retrieve_pieces()
 
